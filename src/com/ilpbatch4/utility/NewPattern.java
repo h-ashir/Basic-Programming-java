f82e4cc83d0ebd692733242beda1f262d494f9e6
ARKathira <athirark18@gmail.com>
2024-06-04 15:32:59 +0530

    new

--- src/com/ilpbatch4/utility/NewPattern.java
+++ src/com/ilpbatch4/utility/NewPattern.java
@@ -5,7 +5,7 @@ public class RightTrianglePattern {
     public static void main(String[] args) {
        
        
-        int rows = 5;
+        int rows = 10;
  
         for (int i = 1; i <= rows; i++) {
             for (int j = 1; j <= i; j++) {
